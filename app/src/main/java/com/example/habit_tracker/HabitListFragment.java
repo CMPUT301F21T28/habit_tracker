2d34c96959db7802c755885a3c175539392e9201
Mingwei Lu <mlu1@ualberta.ca>
2021-10-26 23:03:30 -0600

    *NEW [back button] add back buttons for all fragments

--- app/src/main/java/com/example/habit_tracker/HabitListFragment.java
+++ app/src/main/java/com/example/habit_tracker/HabitListFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,6 +43,14 @@ public class HabitListFragment extends Fragment {
             public void onClick(View view) {
                 NavController controller = Navigation.findNavController(view);
                 controller.navigate(R.id.action_habitListFragment_to_habitAddFragment);
+            }
+        });
+
+        getView().findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController controller = Navigation.findNavController(view);
+                controller.navigate(R.id.action_habitListFragment_to_habitDetailFragment);
             }
         });
     }

8ba3004e0f650d1bb988ef1b76e8f1f73382aabf
Mingwei Lu <mlu1@ualberta.ca>
2021-10-26 20:40:15 -0600

    *NEW [back button] add the back button to the project

--- app/src/main/java/com/example/habit_tracker/MainActivity.java
+++ app/src/main/java/com/example/habit_tracker/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.habit_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.mainPageFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return super.onSupportNavigateUp();
     }
 }

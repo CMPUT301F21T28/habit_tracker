7e7c32805a5614a7fd43445f68d8bb016c0dae26
Mingwei Lu <mlu1@ualberta.ca>
2021-11-01 12:22:57 -0600

    *MDF [modify homepage] add bottom navigation

--- app/src/main/java/com/example/habit_tracker/MainPageFragment.java
+++ app/src/main/java/com/example/habit_tracker/MainPageFragment.java
@@ -9,14 +9,19 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
-public class MainPageFragment extends Fragment {
+public class MainPageFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
 
     public MainPageFragment() {
@@ -38,13 +43,18 @@ public class MainPageFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getView().findViewById(R.id.mine_button).setOnClickListener(new View.OnClickListener() {
+        getView().findViewById(R.id.button_mine).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavController controller = Navigation.findNavController(view);
                 controller.navigate(R.id.action_mainPageFragment_to_habitListFragment);
             }
         });
+
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 }

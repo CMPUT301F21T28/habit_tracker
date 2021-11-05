5b359ee8089085b16e261d43537872640220b997
Darren <darren.wxc@gmail.com>
2021-11-05 10:27:40 -0600

    writing tests. commit for checking something

--- app/src/test/java/com/example/habit_tracker/EventTest.java
+++ app/src/test/java/com/example/habit_tracker/EventTest.java
@@ -1,24 +1,81 @@
 package com.example.habit_tracker;
 
-import android.os.Parcel;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class EventTest {
-// **** Test these functions
-//    void setEventName(String eventName)
-//    void setEventComment(String Comment)
-//
-//    public String getUsername()
-//    public String getHabitID()
-//    public String getEventID()
-//    public String getName()
-//    public String getComment()
+    Event mockEvent;
 
+    /**
+     * Sets mockEvent to a new Event before each test.
+     */
+    @BeforeEach
+    public void createMockEvent(){
+        mockEvent = new Event("mockUsername", "mockHabitId", "mockEventId", "mockName", "mockComment");
+    }
 
-    // Supposed to fail: (function stub)
-//    public int describeContents()
+    /**
+     * Testing the username getter of Event
+     */
+    @Test
+    public void getUsernameTest() {
+        assertEquals(mockEvent.getUsername(), "mockUsername");
+    }
 
-    // Dont have to test because part of superclass
-    //    public void writeToParcel(Parcel parcel, int i)
+    /**
+     * Testing the habitId getter of Event
+     */
+    @Test
+    public void getHabitIDTest() {
+        assertEquals(mockEvent.getHabitID(), "mockHabitId");
+    }
+
+    /**
+     * Testing the eventId getter of Event
+     */
+    @Test
+    public void getEventIDTest() {
+        assertEquals(mockEvent.getEventID(), "mockEventId");
+    }
+
+    /**
+     * Testing the eventName getter of Event
+     */
+    @Test
+    public void getNameTest() {
+        assertEquals(mockEvent.getName(), "mockName");
+    }
+
+    /**
+     * Testing the comment getter of Event
+     */
+    @Test
+    public void getCommentTest() {
+        assertEquals(mockEvent.getComment(), "mockComment");
+    }
+
+    @Test
+    public void setEventNameTest() {
+        assertEquals(mockEvent.getName(), "mockName");
+        mockEvent.setEventName("newMockName");
+        assertEquals(mockEvent.getName(), "newMockName");
+    }
+
+    @Test
+    public void setEventComment() {
+        assertEquals(mockEvent.getComment(), "mockComment");
+        mockEvent.setEventComment("newMockComment");
+        assertEquals(mockEvent.getComment(), "newMockComment");
+    }
+        //    void setEventComment(String Comment)
+    //
+        // Supposed to fail: (function stub)
+    //    public int describeContents()
+
+        // Dont have to test because part of superclass
+        //    public void writeToParcel(Parcel parcel, int i)
 
 
 }

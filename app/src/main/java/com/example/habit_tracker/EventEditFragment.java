3b925b4f4a2f9ff3aba0baf7eca92ec9cefb634d
SonghaoYin <songhao@ualberta.ca>
2021-11-21 18:59:33 -0700

    *NEW [Event Picture] use camera to get picture and to edit image

--- app/src/main/java/com/example/habit_tracker/EventEditFragment.java
+++ app/src/main/java/com/example/habit_tracker/EventEditFragment.java
@@ -1,8 +1,16 @@
 package com.example.habit_tracker;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -10,11 +18,13 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +34,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 import java.util.HashMap;
 
 
@@ -31,6 +43,7 @@ public class EventEditFragment extends Fragment {
     private Button submit;
     private EditText commentContent;
     private EditText locationContent;
+    private ImageButton imageButton;
     private FirebaseFirestore db;
 
     private String username;
@@ -58,6 +71,7 @@ public class EventEditFragment extends Fragment {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -66,9 +80,39 @@ public class EventEditFragment extends Fragment {
         commentContent = (EditText)getView().findViewById(R.id.CommentContent);
         //locationContent = (EditText)getView().findViewById(R.id.LocationContent);
         submit= (Button) getView().findViewById(R.id.Submit);
+        imageButton = getView().findViewById(R.id.editImageButton);
+        String imageString = event.getEventImage();
+        byte[] bitmapArray;
+        bitmapArray = Base64.getDecoder().decode(imageString);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapArray,0,bitmapArray.length);
+        imageButton.setImageBitmap(bitmap);
 
 
         commentContent.setText(event.getEventComment());
+
+        ActivityResultLauncher<Intent> activityResultLauncher;
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        //
+                        Bundle b = result.getData().getExtras();
+                        Bitmap bitmap = (Bitmap) b.get("data");
+                        //bit = (Bitmap) b.get("data");
+                        //imageBitmap = (Bitmap) b.get("data");
+                        imageButton.setImageBitmap(bitmap);
+                    }
+                });
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //file[0] = new File(Environment.getExternalStorageDirectory(),System.currentTimeMillis()+habit+".jpg")
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//EXTRA_OUTPUT, Uri.fromFile(file[0]));
+                activityResultLauncher.launch(intent);
+
+            }
+        });
+
         //locationContent.setText(habitevent.getEventLocation());
 
 
@@ -84,7 +128,7 @@ public class EventEditFragment extends Fragment {
                     commentContent.setError("Comment may not valid. Please ensure that it is between 0 and 20 characters.");
                     return;
                 }
-//                TODO location & image
+//                TODO location
 //                if (20 <= locationContent.getText().toString().length()) {
 //                    isValid[0] = false;
 //                    locationContent.setError("Location may not valid. Please ensure that it is between 0 and 20 characters.");
@@ -100,6 +144,13 @@ public class EventEditFragment extends Fragment {
 
                     event.setEventComment(commentContent.getText().toString());
                     //habitevent.setEventLocation(locationContent.getText().toString());
+                    Bitmap imageBitmap = ((BitmapDrawable) imageButton.getDrawable()).getBitmap();
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    imageBitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+                    byte[] imageByte = byteArrayOutputStream.toByteArray();
+                    String imageString = Base64.getEncoder().encodeToString(imageByte);
+                    event.setEventImage(imageString);
+                    data.put("event image", imageString);
 
                     data.put("event name", event.getEventName());
                     collectionReference

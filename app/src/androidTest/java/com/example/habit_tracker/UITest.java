63b459c7efc98b4e83f5a4b6602a61935cdbbd79
Darren <darren.wxc@gmail.com>
2021-11-28 21:23:13 -0700

    Commit for FF

--- app/src/androidTest/java/com/example/habit_tracker/UITest.java
+++ app/src/androidTest/java/com/example/habit_tracker/UITest.java
@@ -96,130 +96,132 @@ public class UITest {
         // Check if entered into habitlistfragment
         assertNotNull(solo.getView(R.id.habit_list_constraint_layout));
 
-//        // **** Creating, Viewing, Editing Habit
-//        //create a habit
-//        solo.clickOnView(solo.getView(R.id.add_habit_button));
-//        assertNotNull(solo.getView(R.id.habit_add_constraint_layout));
-//
-//        //set details for habit
-//        solo.enterText((EditText) solo.getView(R.id.editText_habitTitle), "robotiumHabit");
-//        solo.enterText((EditText) solo.getView(R.id.editText_habitReason), "robotiumHabitReason");
-//        solo.clickOnView(solo.getView(R.id.textDatePicker));
-//        solo.setDatePicker(0, 2012, 2, 16);
-//        solo.clickOnText("OK");
-//        solo.clickOnView(solo.getView(R.id.textView_select_day));
-//        solo.clickInList(1);
-//        solo.clickOnText("OK");
-//        solo.clickOnView(solo.getView(R.id.radioYes));
-//        solo.clickOnView(solo.getView(R.id.submit_button));
-//
-//        // Check current page as habit list
-//        assertNotNull(solo.getView(R.id.habit_list_constraint_layout));
-//
-//        // Viewing habit
-//        solo.clickOnText("robotiumHabit");
-//        assertNotNull(solo.getView(R.id.habit_detail_layout));
-//        TextView view = (TextView)solo.getView(R.id.textView_habitTitle);
-//        assertEquals("robotiumHabit",view.getText().toString());
-//        view = (TextView)solo.getView(R.id.textView_habitReason);
-//        assertEquals("robotiumHabitReason",view.getText().toString());
-//        view = (TextView)solo.getView(R.id.textView_dateOfStarting);
-//        assertEquals("2012-3-16",view.getText().toString());
-//        view = (TextView)solo.getView(R.id.textView_repeat);
-//        assertEquals("Monday",view.getText().toString());
-//        view = (TextView)solo.getView(R.id.textView_private);
-//        assertEquals("true",view.getText().toString());
-//
-//        // Editing habit
-//        solo.clickOnButton("Edit");
-//        solo.clearEditText((EditText) solo.getView(R.id.editText_habitReason2));
-//        solo.enterText((EditText) solo.getView(R.id.editText_habitReason2), "robotiumHabitReasonNew");
-//        solo.clickOnView(solo.getView(R.id.textDateStarting));
-//        solo.setDatePicker(0, 2013, 2, 16);
-//        solo.clickOnText("OK");
-//        solo.clickOnView(solo.getView(R.id.textView_select_day2));
-//        solo.clickInList(2);
-//        solo.clickOnText("OK");
-//        solo.clickOnView(solo.getView(R.id.radioNo));
-//        solo.clickOnView(solo.getView(R.id.button_submit));
-//
-//        // Check current page as habit list
-//        assertNotNull(solo.getView(R.id.habit_list_constraint_layout));
-//
-//        //Click on the new habit
-//        solo.clickOnText("robotiumHabit");
-//        // Check current page as habit detail
-//        assertNotNull(solo.getView(R.id.habit_detail_layout));
-//
-//        //Check newly updated habit
-//        assertNotNull(solo.getView(R.id.habit_detail_layout));
-//        view = (TextView)solo.getView(R.id.textView_habitTitle);
-//        assertEquals("robotiumHabit",view.getText().toString());
-//        view = (TextView)solo.getView(R.id.textView_habitReason);
-//        assertEquals("robotiumHabitReasonNew",view.getText().toString());
-//        view = (TextView)solo.getView(R.id.textView_dateOfStarting);
-//        assertEquals("2013-3-16",view.getText().toString());
-//        view = (TextView)solo.getView(R.id.textView_repeat);
-//        assertEquals("Tuesday",view.getText().toString());
-//        view = (TextView)solo.getView(R.id.textView_private);
-//        assertEquals("false",view.getText().toString());
-//        // back out from habit details
-//        solo.clickOnActionBarHomeButton();
-//
-//
-//        // **** Creating new habit event
-//        // Add an event
-//        solo.clickOnView(solo.getView(R.id.habit_progress_row));
-//        assertNotNull(solo.getView(R.id.event_list_layout));
-//
-//        // Add an event
-//        solo.clickOnView(solo.getView(R.id.floatingActionButtonAdd));
-//        // Check current page as event add page
-//        assertNotNull(solo.getView(R.id.event_add_constraint_layout));
-//
-//        // Add event detail
-//        solo.enterText((EditText) solo.getView(R.id.editTextName), "robotiumHabitEvent");
-//
-//        solo.clickOnButton("SUBMIT");
-//        assertNotNull(solo.getView(R.id.event_list_layout));
-//        assertTrue(solo.waitForText("robotiumHabitEvent", 1 ,5));
-//
-//        // Click on new habit event
-//        solo.clickOnText("robotiumHabitEvent");
-//        // Check current page as habit event detail
-//        assertNotNull(solo.getView(R.id.habit_event_detail_constrant_layout));
-//        view = (TextView)solo.getView(R.id.textView_detail_eventName_view);
-//        assertEquals("robotiumHabitEvent",view.getText().toString());
-//
-//        // Edit event
-//        solo.clickOnButton("Edit");
-//        assertNotNull(solo.getView(R.id.event_edit_constraint_layout));
-//        // Add event detail
-//        solo.clearEditText((EditText) solo.getView(R.id.nameContent));
-//        solo.enterText((EditText) solo.getView(R.id.nameContent), "robotiumEventNew");
-//        solo.clickOnButton("Submit");
-//        assertNotNull(solo.getView(R.id.event_list_layout));
-//        assertTrue(solo.waitForText("robotiumEventNew"));
-//        solo.clickOnActionBarHomeButton();
-//
-//        // Swipe to delete current habit
-//        // https://stackoverflow.com/questions/24664730/writing-a-robotium-test-to-swipe-open-an-item-on-a-swipeable-listview
-//        int fromX, toX, fromY, toY;
-//        int[] location = new int[2];
-//        View row = solo.getText("robotiumHabit");
-//        row.getLocationInWindow(location);
-//        fromX = location[0];
-//        fromY = location[1];
-//
-//        toX = location[0]+100;
-//        toY = fromY;
-//
-//        solo.drag(fromX, toX, fromY, toY, 10);
-//
-//        solo.clickOnText("YES");
-//        // Confirm habit is deleted from list
-//        assertFalse(solo.waitForText("robotiumHabit", 1, 5));
-//
+        // **** Creating, Viewing, Editing Habit
+        //create a habit
+        solo.clickOnView(solo.getView(R.id.add_habit_button));
+        assertNotNull(solo.getView(R.id.habit_add_constraint_layout));
+
+        //set details for habit
+        solo.enterText((EditText) solo.getView(R.id.editText_habitTitle), "robotiumHabit");
+        solo.enterText((EditText) solo.getView(R.id.editText_habitReason), "robotiumHabitReason");
+        solo.clickOnView(solo.getView(R.id.textDatePicker));
+        solo.setDatePicker(0, 2012, 2, 16);
+        solo.clickOnText("OK");
+        solo.clickOnView(solo.getView(R.id.textView_select_day));
+        solo.clickInList(1);
+        solo.clickOnText("OK");
+        solo.clickOnView(solo.getView(R.id.radioYes));
+        solo.clickOnView(solo.getView(R.id.submit_button));
+
+        // Check current page as habit list
+        assertNotNull(solo.getView(R.id.habit_list_constraint_layout));
+
+        // Viewing habit
+        solo.clickOnText("robotiumHabit");
+        assertNotNull(solo.getView(R.id.habit_detail_layout));
+        TextView view = (TextView)solo.getView(R.id.textView_habitTitle);
+        assertEquals("robotiumHabit",view.getText().toString());
+        view = (TextView)solo.getView(R.id.textView_habitReason);
+        assertEquals("robotiumHabitReason",view.getText().toString());
+        view = (TextView)solo.getView(R.id.textView_dateOfStarting);
+        assertEquals("2012-3-16",view.getText().toString());
+        view = (TextView)solo.getView(R.id.textView_repeat);
+        assertEquals("Monday",view.getText().toString());
+        view = (TextView)solo.getView(R.id.textView_private);
+        assertEquals("true",view.getText().toString());
+
+        // Editing habit
+        solo.clickOnButton("Edit");
+        solo.clearEditText((EditText) solo.getView(R.id.editText_habitReason2));
+        solo.enterText((EditText) solo.getView(R.id.editText_habitReason2), "robotiumHabitReasonNew");
+        solo.clickOnView(solo.getView(R.id.textDateStarting));
+        solo.setDatePicker(0, 2013, 2, 16);
+        solo.clickOnText("OK");
+        solo.clickOnView(solo.getView(R.id.textView_select_day2));
+        solo.clickInList(2);
+        solo.clickOnText("OK");
+        solo.clickOnView(solo.getView(R.id.radioNo));
+        solo.clickOnView(solo.getView(R.id.button_submit));
+
+        // Check current page as habit list
+        assertNotNull(solo.getView(R.id.habit_list_constraint_layout));
+
+        //Click on the new habit
+        solo.clickOnText("robotiumHabit");
+        // Check current page as habit detail
+        assertNotNull(solo.getView(R.id.habit_detail_layout));
+
+        //Check newly updated habit
+        assertNotNull(solo.getView(R.id.habit_detail_layout));
+        view = (TextView)solo.getView(R.id.textView_habitTitle);
+        assertEquals("robotiumHabit",view.getText().toString());
+        view = (TextView)solo.getView(R.id.textView_habitReason);
+        assertEquals("robotiumHabitReasonNew",view.getText().toString());
+        view = (TextView)solo.getView(R.id.textView_dateOfStarting);
+        assertEquals("2013-3-16",view.getText().toString());
+        view = (TextView)solo.getView(R.id.textView_repeat);
+        assertEquals("Tuesday",view.getText().toString());
+        view = (TextView)solo.getView(R.id.textView_private);
+        assertEquals("false",view.getText().toString());
+        // back out from habit details
+        solo.clickOnActionBarHomeButton();
+
+
+        // **** Creating new habit event
+        // Add an event
+        solo.clickOnView(solo.getView(R.id.habit_progress_row));
+        assertNotNull(solo.getView(R.id.event_list_layout));
+
+        // Add an event
+        solo.clickOnView(solo.getView(R.id.floatingActionButtonAdd));
+        // Check current page as event add page
+        assertNotNull(solo.getView(R.id.event_add_constraint_layout));
+
+        // Add event detail
+        solo.enterText((EditText) solo.getView(R.id.editTextName), "robotiumHabitEvent");
+
+        solo.clickOnButton("SUBMIT");
+        assertNotNull(solo.getView(R.id.event_list_layout));
+        assertTrue(solo.waitForText("robotiumHabitEvent", 1 ,5));
+
+        // Click on new habit event
+        solo.clickOnText("robotiumHabitEvent");
+        // Check current page as habit event detail
+        assertNotNull(solo.getView(R.id.habit_event_detail_constrant_layout));
+        view = (TextView)solo.getView(R.id.textView_detail_eventName_view);
+        assertEquals("robotiumHabitEvent",view.getText().toString());
+
+        // Edit event
+        solo.clickOnButton("Edit");
+        assertNotNull(solo.getView(R.id.event_edit_constraint_layout));
+        // Add event detail
+        solo.clearEditText((EditText) solo.getView(R.id.nameContent));
+        solo.enterText((EditText) solo.getView(R.id.nameContent), "robotiumEventNew");
+        solo.clickOnButton("Submit");
+        assertNotNull(solo.getView(R.id.event_list_layout));
+        assertTrue(solo.waitForText("robotiumEventNew"));
+
+        // TODO: FIX THIS - after
+        solo.clickOnActionBarHomeButton();
+
+        // Swipe to delete current habit
+        // https://stackoverflow.com/questions/24664730/writing-a-robotium-test-to-swipe-open-an-item-on-a-swipeable-listview
+        int fromX, toX, fromY, toY;
+        int[] location = new int[2];
+        View row = solo.getText("robotiumHabit");
+        row.getLocationInWindow(location);
+        fromX = location[0];
+        fromY = location[1];
+
+        toX = location[0]+100;
+        toY = fromY;
+
+        solo.drag(fromX, toX, fromY, toY, 10);
+
+        solo.clickOnText("YES");
+        // Confirm habit is deleted from list
+        assertFalse(solo.waitForText("robotiumHabit", 1, 5));
+
 
         // **** Friend Stuff
         solo.clickOnView((FloatingActionButton) solo.getView(R.id.friend_button));
@@ -246,7 +248,7 @@ public class UITest {
         assertFalse(solo.waitForText("testRequest1Name", 1, 5));
 
         // wait for snackbar to disappear (10 second wait)
-        solo.sleep(10000);
+        solo.sleep(5000);
 
         // **** Search and add friend
         solo.clickOnView((FloatingActionButton) solo.getView(R.id.add_friend_button));
@@ -265,6 +267,7 @@ public class UITest {
         solo.clickOnActionBarHomeButton();
         solo.clickOnActionBarHomeButton();
         solo.clickOnActionBarHomeButton();
+        solo.clickOnActionBarHomeButton();
         assertNotNull(solo.getView(R.id.login_frag_linear_layout));
         solo.clearEditText((EditText) solo.getView(R.id.username));
         solo.clearEditText((EditText) solo.getView(R.id.password));
@@ -276,7 +279,7 @@ public class UITest {
         assertNotNull(solo.getView(R.id.friend_list_constraint_layout));
 
         // check if request is there
-        assertFalse(solo.waitForText("robotiumName", 1, 5));
+        assertFalse(solo.waitForText("robotiumName(robotiumUser)", 1, 5));
     }
 
     private void addRequest(String currentUsername, String newUsername, String newRealname) {
